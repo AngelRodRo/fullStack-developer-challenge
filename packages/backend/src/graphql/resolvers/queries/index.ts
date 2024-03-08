72875047a68b1fbb60b8cba97b1820f784c3da74
Angel <aleangrodriguez123@gmail.com>
2024-03-08 10:11:22 -0500

    feat: add user pagination implementation

--- packages/backend/src/graphql/resolvers/queries/index.ts
+++ packages/backend/src/graphql/resolvers/queries/index.ts
@@ -1,7 +1,10 @@
+import { type QueryResolvers } from '../../../__generated__/graphql';
 import * as userQueries from './user';
 
+const queries: QueryResolvers = {
+  ...userQueries
+};
+
 export default {
-  Query: {
-    ...userQueries
-  }
+  Query: queries
 };
--- packages/backend/src/graphql/resolvers/queries/user.ts
+++ packages/backend/src/graphql/resolvers/queries/user.ts
@@ -1,8 +1,17 @@
+import { type QueryUsersArgs, type UserList } from '../../../__generated__/graphql';
 import { UserService } from '../../../services/user';
 
 const userService = new UserService();
 
-export const user = async (): Promise<Array<{ name: string }>> => {
-  const users = await userService.getAll();
-  return users;
+const DEFAULT_TAKE = 10;
+const DEFAULT_SKIP = 0;
+
+export const users = async (parent: any, { take, skip, filter }: QueryUsersArgs): Promise<UserList> => {
+  const takeParam = take !== null ? take : DEFAULT_TAKE;
+  const skipParam = skip !== null ? skip : DEFAULT_SKIP;
+
+  const users = await userService.getAll({ take: takeParam, skip: skipParam });
+  const count = await userService.countAll();
+
+  return { total: count, items: users };
 };
