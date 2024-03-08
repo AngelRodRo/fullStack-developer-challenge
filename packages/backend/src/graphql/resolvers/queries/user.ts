49facefc5f9c161afae7bd7b5820f8d28f317100
Angel <aleangrodriguez123@gmail.com>
2024-03-08 10:47:33 -0500

    feat: use datasources from context for graphql queries

--- packages/backend/src/graphql/resolvers/queries/user.ts
+++ packages/backend/src/graphql/resolvers/queries/user.ts
@@ -1,17 +1,15 @@
+import { type MyContext } from '../../../..';
 import { type QueryUsersArgs, type UserList } from '../../../__generated__/graphql';
-import { UserService } from '../../../services/user';
-
-const userService = new UserService();
 
 const DEFAULT_TAKE = 10;
 const DEFAULT_SKIP = 0;
 
-export const users = async (parent: any, { take, skip, filter }: QueryUsersArgs): Promise<UserList> => {
+export const users = async (parent: any, { take, skip, filter }: QueryUsersArgs, { dataSources }: MyContext): Promise<UserList> => {
   const takeParam = take !== null ? take : DEFAULT_TAKE;
   const skipParam = skip !== null ? skip : DEFAULT_SKIP;
 
-  const users = await userService.getAll({ take: takeParam, skip: skipParam });
-  const count = await userService.countAll();
+  const users = await dataSources.userService.getAll({ take: takeParam, skip: skipParam });
+  const count = await dataSources.userService.countAll();
 
   return { total: count, items: users };
 };
