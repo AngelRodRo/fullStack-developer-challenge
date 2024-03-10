abbfc9447c675f2db06b01d5e763703654d3e555
Angel <aleangrodriguez123@gmail.com>
2024-03-10 12:17:26 -0500

    feat: add user columns hook

---
+++ packages/frontend/src/components/UserManagement/hooks/useUserColumns.tsx
@@ -0,0 +1,56 @@
+import { createColumnHelper } from '@tanstack/react-table';
+import { User } from '@/__generated__/graphql';
+import { useCallback, useMemo } from 'react';
+
+export interface UserColumns extends User {
+  actions: string;
+}
+
+interface UserColumnsOptions {
+  handleSelectedUser: (user: User) => void;
+  handleRemove: (id: number) => void;
+}
+
+export const useUserColumns = ({ handleRemove, handleSelectedUser }: UserColumnsOptions) => {
+  const columnHelper = createColumnHelper<UserColumns>();
+
+  const renderActions = useCallback(
+    (user: User) => {
+      return (
+        <div>
+          <div onClick={() => handleSelectedUser(user)}>Edit</div>
+          {user.id && (
+            <div onClick={() => handleRemove(user.id ?? 0)}>Delete</div>
+          )}
+        </div>
+      );
+    },
+    [handleRemove, handleSelectedUser],
+  );
+
+  return [
+    columnHelper.accessor('name', {
+      cell: (info) => info.getValue(),
+      footer: (info) => info.column.id,
+    }),
+    columnHelper.accessor('lastName', {
+      cell: (info) => info.getValue(),
+      footer: (info) => info.column.id,
+    }),
+    columnHelper.accessor('email', {
+      cell: (info) => info.getValue(),
+      footer: (info) => info.column.id,
+    }),
+    columnHelper.accessor('phone', {
+      cell: (info) => info.getValue(),
+      footer: (info) => info.column.id,
+    }),
+    columnHelper.accessor('address', {
+      cell: (info) => info.getValue(),
+      footer: (info) => info.column.id,
+    }),
+    columnHelper.accessor('actions', {
+      cell: (info) => renderActions(info.row.original),
+    }),
+  ];
+};
