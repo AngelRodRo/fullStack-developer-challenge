d0f6ffb2c77f20f5a17d4376a519b4572db9825f
Angel <aleangrodriguez123@gmail.com>
2024-03-10 12:13:40 -0500

    feat: add graphql mutations

---
+++ packages/frontend/src/graphql/mutations.ts
@@ -0,0 +1,44 @@
+import { graphql } from '@/__generated__';
+
+export const LOGIN = graphql(`
+    mutation login($userInput: UserCredentials!) {
+        login(userCredentials: $userInput) {
+            user {
+                id
+                email
+            }
+            token
+        }
+    }
+`);
+
+
+export const CREATE_USER = graphql(`
+  mutation CreateUser($userInput: UserCreateInput!) {
+    createUser(userInput: $userInput) {
+      email
+      name
+      lastName
+      address
+      phone
+    }
+  }
+`);
+
+export const EDIT_USER = graphql(`
+  mutation EditUser($id: Int!, $userInput: UserEditInput!) {
+    editUser(id: $id, userInput: $userInput) {
+      email
+      name
+      lastName
+      address
+      phone
+    }
+  }
+`);
+
+export const REMOVE_USER = graphql(`
+  mutation RemoveUser($id: Int!) {
+    removeUser(id: $id)
+  }
+`);
