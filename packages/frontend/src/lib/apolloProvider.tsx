5505436e98cf6cb0bcd8a8946fe0729045d3f00c
Angel <aleangrodriguez123@gmail.com>
2024-03-10 12:14:16 -0500

    feat: add apollo provider

---
+++ packages/frontend/src/lib/apolloProvider.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { ApolloLink, HttpLink } from '@apollo/client';
+import {
+  NextSSRApolloClient,
+  ApolloNextAppProvider,
+  NextSSRInMemoryCache,
+  SSRMultipartLink,
+} from '@apollo/experimental-nextjs-app-support/ssr';
+import { setContext } from '@apollo/client/link/context';
+import { AUTH_TOKEN } from '@/constants';
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+function makeClient() {
+  const httpLink = new HttpLink({
+    uri: 'http://localhost:4000',
+  });
+
+  return new NextSSRApolloClient({
+    cache: new NextSSRInMemoryCache(),
+    link:
+      typeof window === 'undefined'
+        ? ApolloLink.from([
+            new SSRMultipartLink({
+              stripDefer: true,
+            }),
+            httpLink,
+            authLink,
+          ])
+        : authLink.concat(httpLink),
+  });
+}
+
+export function ApolloWrapper({ children }: React.PropsWithChildren) {
+  return (
+    <ApolloNextAppProvider makeClient={makeClient}>
+      {children}
+    </ApolloNextAppProvider>
+  );
+}
