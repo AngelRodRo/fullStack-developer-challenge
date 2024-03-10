c8f4d3888934a7128178596f2dbc5e6c3c6e1fec
Angel <aleangrodriguez123@gmail.com>
2024-03-10 12:18:06 -0500

    feat: add edit user modal

---
+++ packages/frontend/src/components/UserManagement/EditUserForm.tsx
@@ -0,0 +1,54 @@
+import { Input } from '@/components/Input';
+import { useForm } from 'react-hook-form';
+import React, { useCallback } from 'react';
+import { Button } from '@/components/Button';
+import {User, UserEditInput} from "@/__generated__/graphql";
+
+type EditUserFormData = {
+  name: string;
+  lastName: string;
+  address: string;
+  phone: string;
+};
+
+interface EditUserFormProps {
+  user: User;
+  onEditUser: (id: number, user: UserEditInput) => void;
+}
+
+export const EditUserForm: React.FC<EditUserFormProps> = ({ user, onEditUser }) => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<EditUserFormData>({
+    defaultValues: {
+      name: user.name,
+      lastName: user.lastName,
+      phone: user.phone,
+      address: user.address
+    }
+  });
+
+  const onSubmit = useCallback(
+    async (formData: EditUserFormData) => {
+      if (user.id) {
+        onEditUser(user.id, formData)
+      }
+    },
+    [onEditUser, user.id],
+  );
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input data-testid="input-name" label="Name" {...register('name', { required: true })} />
+        <Input data-testid="input-lastname" label="LastName" {...register('lastName', { required: true })} />
+        <Input data-testid="input-address" label="Address" {...register('address', { required: true })} />
+        <Input data-testid="input-phone" label="Phone" {...register('phone', { required: true })} />
+
+        <Button type="submit">Edit</Button>
+      </form>
+    </div>
+  );
+};
---
+++ packages/frontend/src/components/UserManagement/EditUserModal.tsx
@@ -0,0 +1,22 @@
+import { Modal } from '@/components/Modal';
+import React, { useMemo } from 'react';
+import {User, UserEditInput} from "@/__generated__/graphql";
+import {EditUserForm} from "@/components/UserManagement/EditUserForm";
+
+interface Props {
+  user: User;
+  onEditUser: (id: number, data: UserEditInput) => void;
+  onClose: () => void;
+}
+
+export const EditUserModal: React.FC<Props> = ({ onClose, onEditUser, user }) => {
+  const renderedHeader = useMemo(
+    () => <h1 className="text-2xl">Create a new user</h1>,
+    [],
+  );
+  return (
+    <Modal header={renderedHeader} onClose={onClose}>
+      <EditUserForm onEditUser={onEditUser} user={user} />
+    </Modal>
+  );
+};
