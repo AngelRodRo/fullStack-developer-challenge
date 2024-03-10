d92bdae73cafb124443aeee89f8fc2c955b5e941
Angel <aleangrodriguez123@gmail.com>
2024-03-10 12:12:24 -0500

    feat: add button props

--- packages/frontend/src/components/Button.tsx
+++ packages/frontend/src/components/Button.tsx
@@ -6,8 +6,8 @@ interface Props extends React.ComponentPropsWithoutRef<'button'> {}
 export const Button: React.FC<Props> = ({ className, children, ...props }) => {
   return (
     <button
-      {...props}
       type="button"
+      {...props}
       className={cn(
         'transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-fit px-5 py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block',
         className,
