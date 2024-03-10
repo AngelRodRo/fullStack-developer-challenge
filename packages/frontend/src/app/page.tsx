dfc21ad8dbbeedfd51a4b0d4c62e0187dc6a519a
Angel <aleangrodriguez123@gmail.com>
2024-03-10 12:20:45 -0500

    feat: add redirection if user is already authenticated

--- packages/frontend/src/app/page.tsx
+++ packages/frontend/src/app/page.tsx
@@ -1,5 +1,19 @@
+"use client"
 import LoginForm from '@/components/LoginForm';
+import { useAuth } from '@/hooks/useAuth';
+import { navigate } from '@/utils/actions';
 
 export default function Home() {
+  const { authenticated } = useAuth();
+
+  if (authenticated === undefined) {
+    return null;
+  }
+
+  if (authenticated) {
+    navigate('/dashboard')
+    return
+  }
+
   return <LoginForm />;
 }
