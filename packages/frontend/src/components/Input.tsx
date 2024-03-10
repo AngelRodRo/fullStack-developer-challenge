46b789d0b229e17968fc1c6140142f0a4145c2d3
Angel <aleangrodriguez123@gmail.com>
2024-03-10 12:12:41 -0500

    feat: add input ref for hook forms

--- packages/frontend/src/components/Input.tsx
+++ packages/frontend/src/components/Input.tsx
@@ -1,19 +1,25 @@
-interface Props {
+import React, { forwardRef } from 'react';
+
+interface Props extends React.ComponentPropsWithoutRef<'input'> {
   label?: string;
 }
 
-export const Input: React.FC<Props> = ({ label }) => {
-  return (
-    <>
-      {label && (
-        <label className="font-semibold text-sm text-gray-600 pb-1 block">
-          {label}
-        </label>
-      )}
-      <input
-        type="text"
-        className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
-      />
-    </>
-  );
-};
+// eslint-disable-next-line react/display-name
+export const Input: React.FC<Props> = forwardRef<HTMLInputElement, Props>(
+  ({ label, ...props }, ref) => {
+    return (
+      <>
+        {label && (
+          <label htmlFor={props.name} className="font-semibold text-sm text-gray-600 pb-1 block">
+            {label}
+          </label>
+        )}
+        <input
+          {...props}
+          ref={ref}
+          className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+        />
+      </>
+    );
+  },
+);
