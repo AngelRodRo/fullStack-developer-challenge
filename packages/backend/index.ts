5f30190690230336cd868ddd8e07ad9ac83258fe
Angel <aleangrodriguez123@gmail.com>
2024-03-09 22:28:08 -0500

    feat: add permissions for routes

--- packages/backend/index.ts
+++ packages/backend/index.ts
@@ -1,27 +1,41 @@
+import 'dotenv/config';
+
 import { ApolloServer, type BaseContext } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { readFileSync } from 'fs';
 
 import resolvers from './src/graphql/resolvers';
 import { UserService } from './src/services/user';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { applyMiddleware } from 'graphql-middleware';
+import { permissions } from './src/permissions';
 
-const typeDefs = readFileSync('./src/graphql/schema.graphql', { encoding: 'utf-8' });
+const typeDefs = readFileSync('./src/graphql/schema.graphql', {
+  encoding: 'utf-8'
+});
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+const schemaWithPermissions = applyMiddleware(schema, permissions);
 
 export interface MyContext extends BaseContext {
-  token?: string | string[] | undefined
+  token?: string
   dataSources: {
     userService: UserService
   }
 }
 
 const server = new ApolloServer<MyContext>({
-  typeDefs,
-  resolvers
+  schema: schemaWithPermissions
 });
 
 void startStandaloneServer(server, {
   context: async ({ req }) => ({
-    token: req.headers.token,
+    token: req.headers.authorization,
     dataSources: {
       userService: new UserService()
     }
