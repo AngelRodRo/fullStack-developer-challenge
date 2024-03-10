51b79a195eb6b98dd5c2ae6fc0d31b74c9958991
Angel <aleangrodriguez123@gmail.com>
2024-03-10 12:18:36 -0500

    feat: add isAuth HOC to check authorization

---
+++ packages/frontend/src/components/isAuth.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { useEffect } from "react";
+import { useAuth } from '@/hooks/useAuth';
+import { navigate } from '@/utils/actions';
+
+export default function isAuth(Component: any) {
+  return function IsAuth(props: any) {
+    const { authenticated  } = useAuth();
+
+    useEffect(() => {
+      if (authenticated === undefined) {
+        return;
+      }
+
+      if (!authenticated) {
+        navigate('/');
+        return;
+      }
+    }, [authenticated]);
+
+
+    if (!authenticated) {
+      return null;
+    }
+
+    return <Component {...props} />;
+  };
+}
