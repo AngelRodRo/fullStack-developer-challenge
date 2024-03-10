057591054b76704209ca0fbf6a194cc672d9b52e
Angel <aleangrodriguez123@gmail.com>
2024-03-10 12:09:11 -0500

    chore: format file

--- packages/backend/src/validators/schemas.ts
+++ packages/backend/src/validators/schemas.ts
@@ -1,7 +1,7 @@
 import { object, string, type ObjectSchema } from 'yup';
 import {
   type UserCreateInput,
-  type UserEditInput,
+  type UserEditInput
 } from '../__generated__/graphql';
 
 export const userCreationSchema: ObjectSchema<UserCreateInput> = object({
@@ -10,12 +10,12 @@ export const userCreationSchema: ObjectSchema<UserCreateInput> = object({
   email: string().email().required(),
   address: string().required(),
   password: string().required(),
-  phone: string().required(),
+  phone: string().required()
 });
 
 export const userEditionSchema: ObjectSchema<UserEditInput> = object({
   name: string().required(),
   lastName: string().required(),
   address: string().required(),
-  phone: string().required(),
+  phone: string().required()
 });
