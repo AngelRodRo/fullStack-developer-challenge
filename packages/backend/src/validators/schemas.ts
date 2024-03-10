21e2150ea79514bb28b7e1ca88e64d5f2cb47d63
Angel <aleangrodriguez123@gmail.com>
2024-03-09 22:24:55 -0500

    feat: add validators

--- packages/backend/src/validators/schemas.ts
+++ packages/backend/src/validators/schemas.ts
@@ -1,5 +1,8 @@
 import { object, string, type ObjectSchema } from 'yup';
-import { type UserCreateInput, type UserEditInput } from '../__generated__/graphql';
+import {
+  type UserCreateInput,
+  type UserEditInput,
+} from '../__generated__/graphql';
 
 export const userCreationSchema: ObjectSchema<UserCreateInput> = object({
   name: string().required(),
@@ -7,12 +10,12 @@ export const userCreationSchema: ObjectSchema<UserCreateInput> = object({
   email: string().email().required(),
   address: string().required(),
   password: string().required(),
-  phone: string().required()
+  phone: string().required(),
 });
 
 export const userEditionSchema: ObjectSchema<UserEditInput> = object({
   name: string().required(),
   lastName: string().required(),
   address: string().required(),
-  phone: string().required()
+  phone: string().required(),
 });
