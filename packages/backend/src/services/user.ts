2fb8d8ce8c673e2b8c30709d62ce44e0a8981f1a
Angel <aleangrodriguez123@gmail.com>
2024-03-09 22:27:55 -0500

    feat: add jwt token and login features

--- packages/backend/src/services/user.ts
+++ packages/backend/src/services/user.ts
@@ -1,11 +1,24 @@
 import { type PrismaClient, type User } from '@prisma/client';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import client from '../database';
+import { hash } from '../utils/encryption';
 
 type UserCreateInput = Omit<User, 'id' | 'createdAt' | 'updatedAt'>;
 type UserWithoutPassword = Omit<User, 'password'>;
+interface UserSuccessLoginParams {
+  user: User
+  token: string
+}
 
-const saltRounds = 10;
+if (
+  process.env.JWT_SECRET_KEY === null ||
+  process.env.JWT_SECRET_KEY === undefined
+) {
+  throw new Error('A JWT Secret key is required before initializing the app');
+}
+
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 export class UserService {
   private readonly client: PrismaClient;
@@ -15,10 +28,16 @@ export class UserService {
   }
 
   async countAll (): Promise<number> {
-    return await this.client.user.count();
+    return this.client.user.count();
   }
 
-  async getAll ({ skip, take }: { skip?: number, take?: number }): Promise<UserWithoutPassword[]> {
+  async getAll ({
+    skip,
+    take
+  }: {
+    skip?: number
+    take?: number
+  }): Promise<UserWithoutPassword[]> {
     const users = await this.client.user.findMany({
       select: {
         id: true,
@@ -36,8 +55,15 @@ export class UserService {
     return users;
   }
 
-  async create ({ name, lastName, email, password, address, phone }: UserCreateInput): Promise<User> {
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
+  async create ({
+    name,
+    lastName,
+    email,
+    password,
+    address,
+    phone
+  }: UserCreateInput): Promise<User> {
+    const hashedPassword = await hash(password);
 
     return await this.client.user.create({
       data: {
@@ -51,7 +77,10 @@ export class UserService {
     });
   }
 
-  async update (id: number, { name, lastName, email, password, address, phone }: Partial<User>): Promise<User> {
+  async update (
+    id: number,
+    { name, lastName, email, password, address, phone }: Partial<User>
+  ): Promise<User> {
     return await this.client.user.update({
       where: {
         id
@@ -70,5 +99,26 @@ export class UserService {
   async remove (id: number): Promise<number> {
     const user = await this.client.user.delete({ where: { id } });
     return user.id;
+  }
+
+  async login (
+    email: string,
+    password: string
+  ): Promise<UserSuccessLoginParams> {
+    const user = await this.client.user.findUnique({ where: { email } });
+    if (user == null) {
+      throw new Error('User not found !');
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      throw new Error('Password is not correct');
+    }
+
+    const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET_KEY);
+    return {
+      user,
+      token
+    };
   }
 }
