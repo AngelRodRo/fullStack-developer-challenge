1d5dd176ea50738787aec81b83d90bb6d3478206
Angel <aleangrodriguez123@gmail.com>
2024-03-10 19:24:42 -0500

    fix: remove unused elements

--- packages/frontend/src/components/BasicTable.tsx
+++ packages/frontend/src/components/BasicTable.tsx
@@ -5,7 +5,7 @@ import {
   PaginationState,
   useReactTable,
 } from '@tanstack/react-table';
-import { Dispatch, SetStateAction, useState } from 'react';
+import { Dispatch, SetStateAction } from 'react';
 
 interface Props<T> {
   data: any;
@@ -33,40 +33,8 @@ export const BasicTable = <T,>({
     },
   });
 
-  const totalPages = Math.floor(total / 5);
   return (
     <div>
-      <div className="my-2 flex sm:flex-row flex-col">
-        <div className="flex flex-row mb-1 sm:mb-0">
-          <div className="relative">
-            <select className="appearance-none h-full rounded-l border block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
-              <option>5</option>
-              <option>10</option>
-              <option>20</option>
-            </select>
-            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-              <svg
-                className="fill-current h-4 w-4"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-              >
-                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-              </svg>
-            </div>
-          </div>
-          <div className="relative">
-            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-              <svg
-                className="fill-current h-4 w-4"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-              >
-                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-              </svg>
-            </div>
-          </div>
-        </div>
-      </div>
       <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
         <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
           <table className="min-w-full leading-normal">
