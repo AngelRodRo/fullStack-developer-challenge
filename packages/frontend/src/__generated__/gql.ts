13697030b263b85a1a4616b64c2c74e915b8753b
Angel <aleangrodriguez123@gmail.com>
2024-03-10 21:02:44 -0500

    feat: add generated types

--- packages/frontend/src/__generated__/gql.ts
+++ packages/frontend/src/__generated__/gql.ts
@@ -13,16 +13,11 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  * Therefore it is highly recommended to use the babel or swc plugin for production.
  */
 const documents = {
-  '\n    mutation login($userInput: UserCredentials!) {\n        login(userCredentials: $userInput) {\n            user {\n                id\n                email\n            }\n            token\n        }\n    }\n':
-    types.LoginDocument,
-  '\n  mutation CreateUser($userInput: UserCreateInput!) {\n    createUser(userInput: $userInput) {\n      email\n      name\n      lastName\n      address\n      phone\n    }\n  }\n':
-    types.CreateUserDocument,
-  '\n  mutation EditUser($id: Int!, $userInput: UserEditInput!) {\n    editUser(id: $id, userInput: $userInput) {\n      email\n      name\n      lastName\n      address\n      phone\n    }\n  }\n':
-    types.EditUserDocument,
-  '\n  mutation RemoveUser($id: Int!) {\n    removeUser(id: $id)\n  }\n':
-    types.RemoveUserDocument,
-  '\n  query GET_USERS($take: Int!, $skip: Int!) {\n    users(take: $take, skip: $skip) {\n      items {\n        id\n        name\n        lastName\n        address\n        email\n        phone\n      }\n      total\n    }\n  }\n':
-    types.Get_UsersDocument,
+    "\n  mutation login($userInput: UserCredentials!) {\n    login(userCredentials: $userInput) {\n      user {\n        id\n        email\n      }\n      token\n    }\n  }\n": types.LoginDocument,
+    "\n  mutation CreateUser($userInput: UserCreateInput!) {\n    createUser(userInput: $userInput) {\n      email\n      name\n      lastName\n      address\n      phone\n    }\n  }\n": types.CreateUserDocument,
+    "\n  mutation EditUser($id: Int!, $userInput: UserEditInput!) {\n    editUser(id: $id, userInput: $userInput) {\n      email\n      name\n      lastName\n      address\n      phone\n    }\n  }\n": types.EditUserDocument,
+    "\n  mutation RemoveUser($id: Int!) {\n    removeUser(id: $id)\n  }\n": types.RemoveUserDocument,
+    "\n  query GET_USERS($take: Int!, $skip: Int!) {\n    users(take: $take, skip: $skip) {\n      items {\n        id\n        name\n        lastName\n        address\n        email\n        phone\n      }\n      total\n    }\n  }\n": types.Get_UsersDocument,
 };
 
 /**
@@ -42,37 +37,26 @@ export function graphql(source: string): unknown;
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(
-  source: '\n    mutation login($userInput: UserCredentials!) {\n        login(userCredentials: $userInput) {\n            user {\n                id\n                email\n            }\n            token\n        }\n    }\n',
-): (typeof documents)['\n    mutation login($userInput: UserCredentials!) {\n        login(userCredentials: $userInput) {\n            user {\n                id\n                email\n            }\n            token\n        }\n    }\n'];
+export function graphql(source: "\n  mutation login($userInput: UserCredentials!) {\n    login(userCredentials: $userInput) {\n      user {\n        id\n        email\n      }\n      token\n    }\n  }\n"): (typeof documents)["\n  mutation login($userInput: UserCredentials!) {\n    login(userCredentials: $userInput) {\n      user {\n        id\n        email\n      }\n      token\n    }\n  }\n"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(
-  source: '\n  mutation CreateUser($userInput: UserCreateInput!) {\n    createUser(userInput: $userInput) {\n      email\n      name\n      lastName\n      address\n      phone\n    }\n  }\n',
-): (typeof documents)['\n  mutation CreateUser($userInput: UserCreateInput!) {\n    createUser(userInput: $userInput) {\n      email\n      name\n      lastName\n      address\n      phone\n    }\n  }\n'];
+export function graphql(source: "\n  mutation CreateUser($userInput: UserCreateInput!) {\n    createUser(userInput: $userInput) {\n      email\n      name\n      lastName\n      address\n      phone\n    }\n  }\n"): (typeof documents)["\n  mutation CreateUser($userInput: UserCreateInput!) {\n    createUser(userInput: $userInput) {\n      email\n      name\n      lastName\n      address\n      phone\n    }\n  }\n"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(
-  source: '\n  mutation EditUser($id: Int!, $userInput: UserEditInput!) {\n    editUser(id: $id, userInput: $userInput) {\n      email\n      name\n      lastName\n      address\n      phone\n    }\n  }\n',
-): (typeof documents)['\n  mutation EditUser($id: Int!, $userInput: UserEditInput!) {\n    editUser(id: $id, userInput: $userInput) {\n      email\n      name\n      lastName\n      address\n      phone\n    }\n  }\n'];
+export function graphql(source: "\n  mutation EditUser($id: Int!, $userInput: UserEditInput!) {\n    editUser(id: $id, userInput: $userInput) {\n      email\n      name\n      lastName\n      address\n      phone\n    }\n  }\n"): (typeof documents)["\n  mutation EditUser($id: Int!, $userInput: UserEditInput!) {\n    editUser(id: $id, userInput: $userInput) {\n      email\n      name\n      lastName\n      address\n      phone\n    }\n  }\n"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(
-  source: '\n  mutation RemoveUser($id: Int!) {\n    removeUser(id: $id)\n  }\n',
-): (typeof documents)['\n  mutation RemoveUser($id: Int!) {\n    removeUser(id: $id)\n  }\n'];
+export function graphql(source: "\n  mutation RemoveUser($id: Int!) {\n    removeUser(id: $id)\n  }\n"): (typeof documents)["\n  mutation RemoveUser($id: Int!) {\n    removeUser(id: $id)\n  }\n"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(
-  source: '\n  query GET_USERS($take: Int!, $skip: Int!) {\n    users(take: $take, skip: $skip) {\n      items {\n        id\n        name\n        lastName\n        address\n        email\n        phone\n      }\n      total\n    }\n  }\n',
-): (typeof documents)['\n  query GET_USERS($take: Int!, $skip: Int!) {\n    users(take: $take, skip: $skip) {\n      items {\n        id\n        name\n        lastName\n        address\n        email\n        phone\n      }\n      total\n    }\n  }\n'];
+export function graphql(source: "\n  query GET_USERS($take: Int!, $skip: Int!) {\n    users(take: $take, skip: $skip) {\n      items {\n        id\n        name\n        lastName\n        address\n        email\n        phone\n      }\n      total\n    }\n  }\n"): (typeof documents)["\n  query GET_USERS($take: Int!, $skip: Int!) {\n    users(take: $take, skip: $skip) {\n      items {\n        id\n        name\n        lastName\n        address\n        email\n        phone\n      }\n      total\n    }\n  }\n"];
 
 export function graphql(source: string) {
   return (documents as any)[source] ?? {};
 }
 
-export type DocumentType<TDocumentNode extends DocumentNode<any, any>> =
-  TDocumentNode extends DocumentNode<infer TType, any> ? TType : never;
+export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<  infer TType,  any>  ? TType  : never;
