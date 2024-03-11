401d60d3d18d63dc7855935846bc50e533103f4a
Angel <aleangrodriguez123@gmail.com>
2024-03-10 19:21:00 -0500

    feat: improve layout organization

---
+++ packages/frontend/src/app/dashboard/layout.tsx
@@ -0,0 +1,24 @@
+import { Sidebar } from '@/components/Sidebar';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard'
+}
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <div className="h-screen bg-gray-100 w-full">
+        <div className="mt-10 md:mt-2 w-full mx-auto px-4 sm:px-8">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
+
