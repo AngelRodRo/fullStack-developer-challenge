9e7cce53e04a34560c62ed7648e3f9938a8fb95c
Angel <aleangrodriguez123@gmail.com>
2024-03-10 19:26:01 -0500

    feat: add success and errors message to user table actions

--- packages/frontend/src/components/UserManagement/UsersTable.tsx
+++ packages/frontend/src/components/UserManagement/UsersTable.tsx
@@ -7,7 +7,7 @@ import {
   UserColumns,
   useUserColumns,
 } from '@/components/UserManagement/hooks/useUserColumns';
-import { useCallback, useState } from 'react';
+import { useCallback, useRef, useState } from 'react';
 import { PaginationState } from '@tanstack/react-table';
 import { useModal } from '@/hooks/useModal';
 import { useQuery } from '@apollo/client';
@@ -15,23 +15,15 @@ import { GET_USERS } from '@/graphql/queries';
 import { useUserActions } from '@/components/UserManagement/hooks/useUserActions';
 import { User, UserCreateInput, UserEditInput } from '@/__generated__/graphql';
 import { EditUserModal } from '@/components/UserManagement/EditUserModal';
+import { ClipLoader } from 'react-spinners';
+import { toast } from 'react-toastify';
+import { UNEXPECTED_ERROR } from '@/constants/errorMessages';
 
 export const UsersTable = () => {
   const [pagination, setPagination] = useState<PaginationState>({
     pageIndex: 0,
     pageSize: 5,
   });
-  const [selectedUser, setSelectedUser] = useState<User>();
-  const {
-    isModalVisible: isCreateModalVisible,
-    showModal: showCreateModal,
-    hideModal: hideCreateModal,
-  } = useModal();
-  const {
-    isModalVisible: isEditModalVisible,
-    showModal: showEditModal,
-    hideModal: hideEditModal,
-  } = useModal();
 
   const { loading, error, data, refetch } = useQuery(GET_USERS, {
     variables: {
@@ -40,9 +32,54 @@ export const UsersTable = () => {
     },
   });
 
-  const { handleCreateUser, handleRemove, handleEditUser } = useUserActions({
-    refetch,
-  });
+  const [selectedUser, setSelectedUser] = useState<User>();
+
+  const {
+    isModalVisible: isCreateModalVisible,
+    showModal: showCreateModal,
+    hideModal: hideCreateModal,
+  } = useModal();
+
+  const {
+    isModalVisible: isEditModalVisible,
+    showModal: showEditModal,
+    hideModal: hideEditModal,
+  } = useModal();
+
+  const { handleCreateUser, handleRemove, handleEditUser } = useUserActions();
+
+  const onCreateUser = useCallback(async (data: UserCreateInput) => {
+    try {
+      await handleCreateUser(data);
+      toast.success("User is created successfully!")
+      refetch();
+      hideCreateModal();
+    } catch (e) {
+      toast.error(UNEXPECTED_ERROR);
+    }
+  }, [handleCreateUser, hideCreateModal, refetch]);
+
+  const onEditUser = useCallback(async (id: number, data: UserEditInput) => {
+    try {
+      await handleEditUser(id, data);
+      toast.success("User is edited successfully!")
+      refetch();
+
+      hideEditModal();
+    } catch (e) {
+      toast.error(UNEXPECTED_ERROR);
+    }
+  }, [handleEditUser, hideEditModal, refetch]);
+
+  const onRemoveUser = useCallback(async (id: number) => {
+    try {
+      await handleRemove(id);
+      refetch();
+      toast.success("User is removed successfully!")
+    } catch (e) {
+      toast.error(UNEXPECTED_ERROR);
+    }
+  }, [handleRemove, refetch]);
 
   const handleSelectedUser = useCallback(
     (user: User) => {
@@ -53,12 +90,36 @@ export const UsersTable = () => {
   );
 
   const columns = useUserColumns({
-    handleRemove,
+    handleRemove: onRemoveUser,
     handleSelectedUser,
   });
 
+  const renderTable = useCallback(() => {
+    if (loading) {
+      return (
+        <div className="w-full h-96 flex items-center justify-center">
+          <ClipLoader
+            color="blue"
+            size={150}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      )
+    }
+    if (data) {
+      return <BasicTable<UserColumns>
+        pagination={pagination}
+        setPagination={setPagination}
+        total={data.users.total}
+        data={data.users.items}
+        columns={columns}
+      />
+    }
+  }, [columns, data, loading, pagination])
+
   return (
-    <div className="container mx-auto px-4 sm:px-8">
+    <div className="mx-auto px-4 sm:px-8">
       <div className="py-8">
         <div className="mb-4">
           <h2 className="text-2xl font-semibold leading-tight">Users</h2>
@@ -66,29 +127,18 @@ export const UsersTable = () => {
         {isCreateModalVisible && (
           <CreateUserModal
             onClose={hideCreateModal}
-            onCreateUser={handleCreateUser}
+            onCreateUser={onCreateUser}
           />
         )}
         {isEditModalVisible && selectedUser && (
           <EditUserModal
             user={selectedUser}
             onClose={hideEditModal}
-            onEditUser={handleEditUser}
+            onEditUser={onEditUser}
           />
         )}
-
-        <div className="w-1/5">
-          <Button onClick={showCreateModal}>Create new user</Button>
-        </div>
-        {data && (
-          <BasicTable<UserColumns>
-            pagination={pagination}
-            setPagination={setPagination}
-            total={data.users.total}
-            data={data.users.items}
-            columns={columns}
-          />
-        )}
+        <Button className="w-40" onClick={showCreateModal}>Create new user</Button>
+        {renderTable()}
       </div>
     </div>
   );
